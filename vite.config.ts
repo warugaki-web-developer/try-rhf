21e7949dfcd9676957954f9bca32e0cebca1fac4
warugaki <warugaki.web.developer@gmail.com>
2022-12-06 01:49:16 +0900

    Initial commit | add 開発環境

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+    setupFiles: ['./src/setupTest.ts'],
+  },
+});
